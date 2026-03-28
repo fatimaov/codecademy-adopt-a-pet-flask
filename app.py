d282a5638596b66242890f55d151afe2ac5b745f
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-03-28 14:57:24 +0100

    create the index route

--- app.py
+++ app.py
@@ -2,3 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+  return '''
+  <h1>Adopt a Pet!</h1>
+  <p>Browse through the links below to find your new furry friend:</p>
+  <ul>
+    <li>Dogs</li>
+    <li>Cats</li>
+    <li>Rabbits</li>
+  </ul>
+  '''
